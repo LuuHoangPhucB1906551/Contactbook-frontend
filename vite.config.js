d5f8ceb4ac4b23e9f62e0e6bc95607b4cc8abdd6
Hoang Phuc <lhphuc1602@gmail.com>
2022-11-03 11:14:53 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,7 +8,13 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3001,
-    },
+    proxy: {
+      "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true,
+      },
+      }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
